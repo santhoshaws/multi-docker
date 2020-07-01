34fd564009639307db67ce36b3da821980a49b79
santhoshaws <49520712+santhoshaws@users.noreply.github.com>
2020-07-01 11:59:01 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -69,3 +69,14 @@ app.post('/values', async (req, res) => {
 app.listen(5000, (err) => {
   console.log('Listening');
 });
+<script type="text/javascript">
+var rumMOKey='e287abb41be83cd6da267920d905e7aa';
+(function(){
+if(window.performance && window.performance.timing && window.performance.navigation) {
+	var site24x7_rum_beacon=document.createElement('script');
+	site24x7_rum_beacon.async=true;
+	site24x7_rum_beacon.setAttribute('src','//static.site24x7rum.com/beacon/site24x7rum-min.js?appKey='+rumMOKey);
+	document.getElementsByTagName('head')[0].appendChild(site24x7_rum_beacon);
+}
+})(window)
+</script>
